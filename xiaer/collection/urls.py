63b3d3cbf66bccc62d17b5188a7c8d4147207483
nuty <heizai3@gmail.com>
2014-03-29 15:28:03 +0800

    fix url

--- xiaer/collection/urls.py
+++ xiaer/collection/urls.py
@@ -2,6 +2,6 @@ from xiaer.collection.views import spider, loop, collection
 from xiaer.app import app
 
 
-app.add_url_rule('/', view_func=spider, methods=['GET'])
-app.add_url_rule('/collection', view_func=collection, methods=['GET'])
+app.add_url_rule('/', view_func=collection, methods=['GET'])
+app.add_url_rule('/spider', view_func=spider, methods=['GET'])
 app.add_url_rule('/loop', view_func=loop, methods=['GET','POST'])
